3b83138ff151d0de98fa89cad04f0465f44c8f90
gjdnddd <gjdnddd@gmail.com>
2026-05-17 07:15:03 +0900

    feat: initial app skeleton

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+﻿plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.gjdnd.aicollector"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.gjdnd.aicollector"
+        minSdk = 33
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+}
